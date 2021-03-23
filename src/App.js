292b2f6ca03b448ffe86162f20d799e58dee2f23
Lewis Ward <lewisjohnward+git1@gmail.com>
2021-03-23 17:54:36 +0000

    updated stock display

--- src/App.js
+++ src/App.js
@@ -3,12 +3,10 @@ import Header from "./Header"
 import Navbar from "./Navbar"
 import Footer from "./Footer"
 import StockFrame from "./StockFrame"
+import {stockData} from "./stockdata"
 import "./App.css"
 
 export default function App(){
-  const [stockData, setStockData] = useState([])
-  const [mappedStocks, setMappedStocks] = useState()
-  const [displayStocks, setDisplayStocks] = useState(false)
 
   const stocks = ["AAPL", "TSLA", "MMM"]
 
@@ -24,25 +22,11 @@ export default function App(){
   }
 
   useEffect(() => {
-    for (let i = 0; i < stocks.length; i ++){
-      fetch(`https://${sandbox.api}/stable/stock/${stocks[i]}/quote?token=${sandbox.token}&period=annual/`)
-        .then(response => response.json())
-        .then(data => setStockData(prev => {
-          return [
-            ...prev, data
-          ]
-        }))
-    }
-    setDisplayStocks(true)
-    const mappedStocks1 = stockData.map(item => <p key={item}>test</p>)
-    setMappedStocks(mappedStocks1)
-    console.log("stocks mapped")
+
   }, [])
 
-  console.log(mappedStocks)
 
-
-
+  const mappedData = stockData.map(item => <StockFrame key={item.companyName} data={item}/>)
   
   
 
@@ -52,8 +36,10 @@ export default function App(){
     <div className="main">
       <Navbar />
       <div className="app">
-        <h1>This is the app component</h1>
-        {displayStocks? {mappedStocks}: "no"}
+        <h1 className="component-title">This is the app component</h1>
+        <div className="stockdata-grid">
+          {mappedData}
+        </div>
         <Footer />
       </div>
     </div>
--- src/StockFrame.js
+++ src/StockFrame.js
@@ -2,9 +2,19 @@ import React from "react"
 import "./StockFrame.css"
 
 export default function StockFrame(props){
+
+    const dayChange = (props.data.currentPrice/props.data.dayStartPrice).toFixed(2)
+
+    //if dayChange is > 0 then background color is green
+
+    const style = dayChange > 0 ? "green" : "red"
+
     return (
-        <div className="stock-frame">
-            <p>Test</p>
+        <div className= "stock-frame" style={{backgroundColor: style}}>
+            <h2>{props.data.companyName}</h2>
+            <p>Start price: {props.data.dayStartPrice}</p>
+            <p>Current price: {props.data.currentPrice}</p>
+            <p>day change:{dayChange}</p>
         </div>
     )
 }
